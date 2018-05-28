5f3c1b814ff0876c1665c53de43a1e25428f0ded
Michał Kalisz <228136@student.pwr.edu.pl>
2018-05-28 08:30:56 +0000

    Merged PR 25: ListView and loading screen while fetching offers are implemented and deocorated.
    Related work items: #101

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,15 +2,20 @@ import Vue from 'vue'
 import App from './pages/app/App.vue'
 import BootstrapVue from 'bootstrap-vue'
 import axios from 'axios'
+import Vuetify from 'vuetify'
+import MenuIcon from "vue-material-design-icons/menu.vue"
 
 import router from './router/router'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+
 Vue.config.productionTip = false
 
+Vue.use(MenuIcon)
 Vue.use(BootstrapVue)
 Vue.use(axios)
+Vue.use(Vuetify)
 
 /* eslint-disable */
 new Vue({
--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Search from '@/components/Search'
+import ListView from '@/components/ListView'
 
 Vue.use(Router)
 
 const routes = [
-  { path: '/', component: Search }
-  // { path: '/recyclerview', component: RecyclerView, props: (route) => ({ search: route.query.search }) }
+  { path: '/', component: Search },
+  { path: '/offers', component: ListView, props: (route) => ({ search: route.query.search }) }
 ]
 
 export default new Router({

fcae89d6f1552dbc9fc740b142e5b6fc34ac913e
Michal Kalisz <kfeedu@gmail.com>
2018-06-17 23:44:22 +0200

    Gumtree scrapping implemented

--- backend/src/main/java/zpi/flattery/service/OfferService.java
+++ backend/src/main/java/zpi/flattery/service/OfferService.java
@@ -1,6 +1,8 @@
 package zpi.flattery.service;
 
+import io.reactivex.Single;
 import io.reactivex.schedulers.Schedulers;
+import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.request.async.DeferredResult;
 import zpi.flattery.controllers.model.SearchRequest;
@@ -10,9 +12,11 @@ import zpi.flattery.models.User;
 import zpi.flattery.repository.FavouriteDao;
 import zpi.flattery.repository.OfferDao;
 import zpi.flattery.webscrapper.OfferScrapper;
+import zpi.flattery.webscrapper.strategy.GumtreeStrategy;
 import zpi.flattery.webscrapper.strategy.OlxStrategy;
 
 import javax.annotation.Resource;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,7 +30,7 @@ public class OfferService {
     FavouriteDao favouriteDao;
 
     public void scrapOffers(SearchRequest searchRequest, DeferredResult<List<Offer>> deferredResult){
-        OfferScrapper scrapper = OfferScrapper.Builder(new OlxStrategy())
+        OfferScrapper olxScrapper = OfferScrapper.Builder(new OlxStrategy())
                 .place(searchRequest.getCity())
                 .query(searchRequest.getQuery())
                 .minPrice(searchRequest.getMinPrice())
@@ -37,7 +41,27 @@ public class OfferService {
                 .radius(searchRequest.getRadiusFromLocation())
                 .build();
 
-        scrapper.scrapOffers()
+
+        OfferScrapper gumtreeScrapper = OfferScrapper.Builder(new GumtreeStrategy())
+                .place(searchRequest.getCity())
+                .query(searchRequest.getQuery())
+                .minPrice(searchRequest.getMinPrice())
+                .maxPrice(searchRequest.getMaxPrice())
+                .roomType(searchRequest.getRoomType())
+                .offerType(searchRequest.getOfferType())
+                .maxDaysOld(searchRequest.getOfferDaysOld())
+                .radius(searchRequest.getRadiusFromLocation())
+                .build();
+
+        Single.zip(
+                olxScrapper.scrapOffers().subscribeOn(Schedulers.io()),
+                gumtreeScrapper.scrapOffers().subscribeOn(Schedulers.io()),
+                (a, b) -> {
+                    a.addAll(b);
+                    Collections.sort(a);
+                    return a;
+                }
+        )
         .subscribeOn(Schedulers.io())
         .subscribe(response -> handleScrapperResponse(response, deferredResult),
                 error -> handleScrapperError(error, deferredResult));
--- backend/src/main/java/zpi/flattery/webscrapper/strategy/ScrapStrategy.java
+++ backend/src/main/java/zpi/flattery/webscrapper/strategy/ScrapStrategy.java
@@ -1,15 +1,79 @@
 package zpi.flattery.webscrapper.strategy;
 
+import io.reactivex.Observable;
 import io.reactivex.Single;
+import io.reactivex.schedulers.Schedulers;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 import zpi.flattery.models.Offer;
 import zpi.flattery.models.enums.OfferType;
 import zpi.flattery.models.enums.RoomType;
+import zpi.flattery.webscrapper.googleapi.GeocodeResponse;
+import zpi.flattery.webscrapper.googleapi.GoogleGeocodeApi;
 
-import java.io.IOException;
+import java.text.Normalizer;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
-public interface ScrapStrategy {
-    Single<List<Offer>> scrap();
-    void setScrapParameters(RoomType roomType, OfferType offerType, String query, double minPrice, double maxPrice, String place, Integer radius, Integer daysOld);
+public abstract class ScrapStrategy {
 
+    abstract public Single<List<Offer>> scrap();
+    abstract public void setScrapParameters(RoomType roomType, OfferType offerType, String query, double minPrice, double maxPrice, String place, Integer radius, Integer daysOld);
+
+    protected String getNormalizedString(String location){
+        location = Normalizer.normalize(location, Normalizer.Form.NFD);
+        location = location.replaceAll("\\p{InCombiningDiacriticalMarks}+", "");
+        //'ł' is still not transformed to 'l'
+        location = location.replaceAll("ł", "l");
+        location = location.replaceAll("Ł", "L");
+        location = location.replaceAll("\\s+", "-");
+        return location;
+    }
+
+    protected boolean isOfferFreshEnough(Offer offer, int daysOld) {
+        if (daysOld == 0)
+            return true;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        LocalDateTime parsedDate = LocalDateTime.parse(offer.getPublishingDate(), formatter);
+        LocalDateTime currentDate = LocalDateTime.now();
+        long daysBetween = Duration.between(parsedDate, currentDate).toDays();
+        return daysBetween <= daysOld;
+    }
+
+    protected Single<List<Offer>> addLatLang(ArrayList<Offer> scrappedOffers) {
+        //Creating Retrofit instance
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(GoogleGeocodeApi.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build();
+        GoogleGeocodeApi service = retrofit.create(GoogleGeocodeApi.class);
+
+        //Creating Observable objects from offers and addidng them to list
+        List<Observable<Offer>> observableList = new LinkedList<>();
+        for (Offer offer : scrappedOffers) {
+            observableList.add(service.getGeocode(offer.getCity(), GoogleGeocodeApi.API_KEY)
+                    .map(response -> combineResponseWithOffer(offer, response)));
+        }
+        //Observables from list are observed on the same thread so the whole process will wait for all of them to being processed, before completion
+        return Observable.fromIterable(observableList)
+                .flatMap(task -> {
+                    task.observeOn(Schedulers.computation());
+                    return task;
+                }).toList();
+    }
+
+
+    private Offer combineResponseWithOffer(Offer offer, GeocodeResponse geocodeResponse) {
+        if (!geocodeResponse.status.equals("OVER_QUERY_LIMIT")) {
+            offer.setCoordinates("lat=" + geocodeResponse.results[0].geometry.location.lat + ", lng=" + geocodeResponse.results[0].geometry.location.lng);
+        }
+        return offer;
+    }
 }
